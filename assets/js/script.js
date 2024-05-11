51189add5c467f7b222b25fdb68169886a947dce
Brendan <brendancollins89@hotmail.com>
2024-05-11 13:53:29 +0100

    Add list of all  possible questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,22 @@ function updateTimer() {
 }
 
 const timerInterval = setInterval(updateTimer, 1000);
+
+const questions [
+    {
+        question: "What is Javascript?",
+        answers: ["A programming language", "A database management system", "A markup language", "An operating system"]
+        correctAnswer: 0;
+    }
+    {
+        question: "What is the purpose of the 'typeof' operator in JavaScript?",
+        answers: ["To create a new variable", "To concatenate strings", "To check the data type of a variable", "To define a function"]
+        correctAnswer: 2;
+    }
+    {
+        question: "How do you declare a variable in JavaScript?",
+        answers: ["var", "variable", "let", "v"]
+        correctAnswer: 0;
+    }
+
+]
